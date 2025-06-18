6d08d47a91520f8d832b7c3d97f0e5383f0553f3
Quayyum-a <quayyumariyo@gmail.com>
2025-06-18 11:07:44 +0100

    refactor: update Login.jsx for Tailwind v4 compatibility

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -5,27 +5,25 @@ import PasswordInput from "../../components/input/PasswordInput";
 import { validateEmail, validatePassword } from "../../utils/helper";
 
 const Login = () => {
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
   const handleLogin = (e) => {
     e.preventDefault();
-  
 
-  if(!validateEmail(email)) {
-    setError("Invalid email, please enter a valid email");
-    return;
-  }
+    if (!validateEmail(email)) {
+      setError("Invalid email, please enter a valid email");
+      return;
+    }
 
-  if(!validatePassword(password)) {
-    setError("Invalid password, please enter a valid password");
-    return;
-  }
+    if (!validatePassword(password)) {
+      setError("Invalid password, please enter a valid password");
+      return;
+    }
 
-  setError("");
-}
+    setError("");
+  };
 
   return (
     <>
@@ -35,20 +33,29 @@ const Login = () => {
         <div className="bg-white p-8 rounded-lg shadow-md w-96">
           <form onSubmit={handleLogin}>
             <h4 className="text-2xl mb-7">Login</h4>
-            <input type="text" placeholder="Email" className="input-box" 
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            <input
+              type="text"
+              placeholder="Email"
+              className="input-box"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
-            <PasswordInput value={password} onChange={(e) => setPassword(e.target.value)} />
+            <PasswordInput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
             {error && <p className="text-red-500 text-sm">{error}</p>}
             <button type="submit" className="btn-primary">
               Login
             </button>
 
             <p className="text-sm text-center mt-4">
-              Not registered yet? {" "}
-              <Link to="/signUp" className="text-blue-500 hover:underline font-medium ">
-              Create an Account
+              Not registered yet?{" "}
+              <Link
+                to="/signUp"
+                className="text-blue-500 hover:underline font-medium "
+              >
+                Create an Account
               </Link>
             </p>
           </form>
