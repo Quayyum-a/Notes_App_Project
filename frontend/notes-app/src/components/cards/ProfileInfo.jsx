bfc2a4e6a5c74e0ead07de36c7d6f17ecfa11e5c
Quayyum-a <quayyumariyo@gmail.com>
2025-06-18 11:32:24 +0100

    Add new cards components

---
+++ frontend/notes-app/src/components/cards/ProfileInfo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProfileInfo = ({onLogout}) => {
+  return (
+    <div className='flex items-center gap-2'>
+      <div className="w-12 h-12 bg-primary rounded-full flex items-center justify-center text">TU</div>
+      <div>
+        <p className='text-sm font-medium'>Quayyum</p>
+        <button className='' onClick={onLogout}>Logout</button>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileInfo
