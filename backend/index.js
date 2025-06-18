14ea77420f1047d91645e37688ac2b6bc94554e9
Quayyum-a <quayyumariyo@gmail.com>
2025-06-18 16:58:35 +0100

    Add error handling for missing request body in /create-account route

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,15 @@ require("dotenv").config();
 
 const mongoose = require("mongoose");
 const config = require("./config.json");
+mongoose.connect(config.connectionString);
+
+const User = require("./models/user");
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
+const jwt = require("jsonwebtoken");
+const { authenticateToken } = require("./utilities");
 app.use(express.json());
 
 app.use(
@@ -16,6 +21,55 @@ app.use(
 
 app.get("/", (req, res) => {
   res.json({ data: "Hello World" });
+});
+
+app.post("/create-account", async (req, res) => {
+  if (!req.body) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Request body is required" });
+  }
+  const { fullName, email, password } = req.body;
+
+  if (!fullName) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Full name is required" });
+  }
+  if (!email) {
+    return res.status(400).json({ error: true, message: "Email is required" });
+  }
+  if (!password) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Password is required" });
+  }
+
+  const existingUser = await User.findOne({ email: email });
+  if (existingUser) {
+    return res
+      .status(400)
+      .json({ error: true, message: "User already exists" });
+  }
+  const newUser = new User({ fullName, email, password });
+  await newUser.save();
+
+  const accessToken = jwt.sign(
+    {
+      newUser,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: "36000m",
+    }
+  );
+
+  res.json({
+    error: false,
+    newUser,
+    message: "Registration successful",
+    accessToken,
+  });
 });
 
 app.listen(8000);
