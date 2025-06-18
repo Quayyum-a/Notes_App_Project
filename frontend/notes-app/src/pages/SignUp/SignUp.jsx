5e0b7fb42411e83d1a650ba0d615613c1f95faa1
Quayyum-a <quayyumariyo@gmail.com>
2025-06-19 00:22:00 +0100

    Fix field name mismatch in SignUp component (fullname to fullName)

--- frontend/notes-app/src/pages/SignUp/SignUp.jsx
+++ frontend/notes-app/src/pages/SignUp/SignUp.jsx
@@ -1,80 +1,114 @@
-import React, { useState } from 'react'
-import Navbar from '../../components/Navbar/Navbar'
-import { validateEmail, validatePassword } from '../../utils/helper'
-import { Link } from 'react-router-dom'
-import PasswordInput from '../../components/input/PasswordInput'
+import React, { useState, useEffect } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import { validateEmail, validatePassword } from "../../utils/helper";
+import { Link, useNavigate } from "react-router-dom";
+import PasswordInput from "../../components/input/PasswordInput";
+import axiosInstance from "../../utils/axiosInstance";
 
 const SignUp = () => {
-
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
-  const handleSignUp = (e) => {
+  useEffect(() => {
+    const accessToken = localStorage.getItem("token");
+    if (accessToken) {
+      navigate("/dashboard");
+    }
+  }, [navigate]);
+
+  const handleSignUp = async (e) => {
     e.preventDefault();
 
-    if(!name.trim()) {
+    if (!name.trim()) {
       setError("Name is required");
       return;
     }
 
-    if(!validateEmail(email)) {
+    if (!validateEmail(email)) {
       setError("Invalid email, please enter a valid email");
       return;
     }
 
-    if(!validatePassword(password)) {
+    if (!validatePassword(password)) {
       setError("Invalid password, please enter a valid password");
       return;
     }
 
     setError("");
-  }
-    return (
-      <>
-        <Navbar />
 
-        <div className="flex justify-center items-center mt-28">
-          <div className="bg-white p-8 rounded-lg shadow-md w-96">
-            <form onSubmit={handleSignUp}>
-              <h4 className="text-2xl mb-7">SignUp</h4>
-              <input
-                type="text"
-                placeholder="Name"
-                className="input-box"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-              <input
-                type="text"
-                placeholder="Email"
-                className="input-box"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <PasswordInput
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              {error && <p className="text-red-500 text-sm">{error}</p>}
-              <button type="submit" className="btn-primary">
-                Create Account
-              </button> 
-              <p className="text-sm text-center mt-4">
-                Already registered?{" "}
-                <Link
-                  to="/login"
-                  className="text-blue-500 hover:underline font-medium "
-                >
-                  Login here
-                </Link>
-              </p>
-            </form>
-          </div>
+    try {
+      const response = await axiosInstance.post("/create-account", {
+        fullName: name,
+        email: email,
+        password: password,
+      });
+      if (response.data && response.data.error) {
+        setError(response.data.message);
+        return;
+      }
+      if (response.data && response.data.accessToken) {
+        localStorage.setItem("token", response.data.accessToken);
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
+        setError(error.response.data.message);
+      } else {
+        setError("An unexpected error occurred. Please try again.");
+      }
+    }
+  };
+  return (
+    <>
+      <Navbar />
+
+      <div className="flex justify-center items-center mt-28">
+        <div className="bg-white p-8 rounded-lg shadow-md w-96">
+          <form onSubmit={handleSignUp}>
+            <h4 className="text-2xl mb-7">SignUp</h4>
+            <input
+              type="text"
+              placeholder="Name"
+              className="input-box"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Email"
+              className="input-box"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <PasswordInput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {error && <p className="text-red-500 text-sm">{error}</p>}
+            <button type="submit" className="btn-primary">
+              Create Account
+            </button>
+            <p className="text-sm text-center mt-4">
+              Already registered?{" "}
+              <Link
+                to="/login"
+                className="text-blue-500 hover:underline font-medium "
+              >
+                Login here
+              </Link>
+            </p>
+          </form>
         </div>
-      </>
-    );
-}
+      </div>
+    </>
+  );
+};
 
 export default SignUp;
