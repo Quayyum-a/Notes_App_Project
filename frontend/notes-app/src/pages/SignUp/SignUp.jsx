2881de213c1a64ebe6a396d7c42c8af8acec344c
Quayyum-a <quayyumariyo@gmail.com>
2025-06-18 11:01:13 +0100

    fix: update SignUp component styling

--- frontend/notes-app/src/pages/SignUp/SignUp.jsx
+++ frontend/notes-app/src/pages/SignUp/SignUp.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    if(!validateEmail(email)) {
+      setError("Invalid email, please enter a valid email");
+      return;
+    }
+
+    if(!validatePassword(password)) {
+    return (
+     <>
+      <Navbar />
+
+      <div className="flex justify-center items-center mt-28">
+        <div className="bg-white p-8 rounded-lg shadow-md w-96">
+          <form onSubmit={handleSignUp}>
+            <h4 className="text-2xl mb-7">Login</h4>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
 
