16a69a6fd9fe378b3ab070623d42568d12fb5dae
Quayyum-a <quayyumariyo@gmail.com>
2025-06-19 00:22:15 +0100

    Fix field name consistency in ProfileInfo component (fullname to fullName)

--- frontend/notes-app/src/components/cards/ProfileInfo.jsx
+++ frontend/notes-app/src/components/cards/ProfileInfo.jsx
@@ -5,10 +5,10 @@ const ProfileInfo = ({ userInfo, onLogout }) => {
   return (
     <div className="flex items-center gap-2">
       <div className="w-12 h-12 bg-primary rounded-full flex items-center justify-center text-slate-950 font-medium text-lg bg-slate-100">
-        {getInitials(userInfo?.fullname)}
+        {getInitials(userInfo?.fullName)}
       </div>
       <div>
-        <p className="text-sm font-medium">{userInfo?.fullname}</p>
+        <p className="text-sm font-medium">{userInfo?.fullName}</p>
         <button className="text-sm text-slate-500 underline" onClick={onLogout}>
           Logout
         </button>
