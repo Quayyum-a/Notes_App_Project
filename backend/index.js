84a1368854f978f431f4fceadc69bfb53cdcd4f9
Quayyum-a <quayyumariyo@gmail.com>
2025-06-18 16:27:41 +0100

    Add all changed and untracked files

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,7 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const config = require("./config.json");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -14,6 +18,6 @@ app.get("/", (req, res) => {
   res.json({ data: "Hello World" });
 });
 
-app.listen(8000)
+app.listen(8000);
 
-module.exports = app; 
+module.exports = app;
