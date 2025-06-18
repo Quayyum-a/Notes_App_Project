b43f935f8d427de3b907698878f4103249b5bcf6
Quayyum-a <quayyumariyo@gmail.com>
2025-06-18 11:01:02 +0100

    fix: update Login component styling

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -1,19 +1,46 @@
-import React from "react";
-import Navbar from "../../components/navbar";
+import React, { useState } from "react";
+import Navbar from "../../components/Navbar/Navbar";
 import { Link } from "react-router-dom";
 import PasswordInput from "../../components/input/PasswordInput";
+import { validateEmail, validatePassword } from "../../utils/helper";
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+  
+
+  if(!validateEmail(email)) {
+    setError("Invalid email, please enter a valid email");
+    return;
+  }
+
+  if(!validatePassword(password)) {
+    setError("Invalid password, please enter a valid password");
+    return;
+  }
+
+  setError("");
+}
+
   return (
     <>
       <Navbar />
 
       <div className="flex justify-center items-center mt-28">
         <div className="bg-white p-8 rounded-lg shadow-md w-96">
-          <form onSubmit={() => {}}>
+          <form onSubmit={handleLogin}>
             <h4 className="text-2xl mb-7">Login</h4>
-            <input type="text" placeholder="Email" className="input-box" />
-            <PasswordInput />
+            <input type="text" placeholder="Email" className="input-box" 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            />
+            <PasswordInput value={password} onChange={(e) => setPassword(e.target.value)} />
+            {error && <p className="text-red-500 text-sm">{error}</p>}
             <button type="submit" className="btn-primary">
               Login
             </button>
