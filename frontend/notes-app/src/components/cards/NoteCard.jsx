59eceeb98556aaedd519ed29cd51ad83ab047fb9
Quayyum-a <quayyumariyo@gmail.com>
2025-06-19 10:15:03 +0100

    Update NoteCard.jsx (unspecified changes)

--- frontend/notes-app/src/components/cards/NoteCard.jsx
+++ frontend/notes-app/src/components/cards/NoteCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { MdOutlinePushPin, MdCreate, MdDelete } from "react-icons/md";
+import moment from "moment";
 
 const NoteCard = ({
   title,
@@ -16,13 +17,20 @@ const NoteCard = ({
       <div className="flex items-center justify-between">
         <div>
           <h6 className="text-sm font-medium">{title}</h6>
-          <span className="text-xs text-slate-500">{date}</span>
+          <span className="text-xs text-slate-500">
+            {moment(date).format("DD/MM/YYYY")}
+          </span>
         </div>
-        <MdOutlinePushPin className={`icon-btn ${isPinned ? 'text-green-600' : 'text-slate-500'}`} onClick={onPinNote} />
+        <MdOutlinePushPin
+          className={`icon-btn ${
+            isPinned ? "text-green-600" : "text-slate-500"
+          }`}
+          onClick={onPinNote}
+        />
       </div>
       <p className="text-xs text-slate-500 mt-2">{content?.slice(0, 60)}</p>
       <div className="flex items-center justify-between mt-2">
-        <div className="text-xs text-slate-500">{tags}</div>
+        <div className="text-xs text-slate-500">{tags.map((item) => `#${item}`)}</div>
         <div className="flex items-center gap-2">
           <MdCreate
             className="icon-btn hover:text-green-600"
